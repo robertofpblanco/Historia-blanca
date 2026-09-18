2a61bc0ec6c1267f767a8d54e45908623ee6ad67
robertofpblanco <robertofp1721@gmail.com>
2026-09-18 07:29:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'historia-blanca-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Nunca cachear llamadas a la API: siempre red fresca
+  if (e.request.url.includes('api-sports.io')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
